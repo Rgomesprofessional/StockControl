ea5e7ca6d40ff1f951bec5279c69a74535aba1c3
R. Gomes Professional <r.gomes90@hotmail.com>
2019-03-07 08:09:47 -0300

    Products - Adm - customer - order fix

--- index.php
+++ index.php
@@ -41,26 +41,3 @@
         
     require './footer.php';
 ?>
-
-=======
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        
-        <h1>Stock Control</h1>
-        
-        <?php
-        // put your code here
-        ?>
-    </body>
-</html>
-
